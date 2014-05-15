a5ee6467e03790ccb89713e5fb666f7a72d5d852
rakeybulhasan <rakeybulhasan@gmail.com>
2014-05-15 14:14:11 +0600

    session check

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -76,6 +76,7 @@ class UserController extends BaseController {
     }
     public function getLogout()
     {
+        Session::flush();
         Auth::logout();
         return Redirect::to('/');
     }
